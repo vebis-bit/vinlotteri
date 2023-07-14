a5befc9255b51a60c5e1df8e20cf2b216e4745c7
vebjorn <vebjorn.blom-hagen@kdef.cloud>
2023-07-14 09:35:17 +0200

    fjernet ubrukt kode

--- vinlotteri_moc.py
+++ vinlotteri_moc.py
@@ -2,7 +2,6 @@ import random
 import sys
 import time
 import pygame as py
-import math
 import os
 from python_version.button import Button
 
@@ -10,13 +9,15 @@ from python_version.button import Button
 py.init()
 py.mixer.init()
 start = 1
-stop = 6
+stop = 100
+length = 10
 choises = {
     "Erland": [],
-    "Tobias": [],
+    "Jonas": [],
     "Vebjorn":[],
-    "Andreas": [],
+    "Amund": [],
     "Aslak": [],
+    "Andreas": [],
 }
 ranking = set()
 antall_spillere = len(choises)
@@ -28,7 +29,7 @@ for i in range(start,stop):
     test.append(i)
 
 for key, value in choises.items():
-    for i in range(1):
+    for i in range(0,length):
         choises[key].append(test.pop(random.randint(0,len(test)-1)))
 
 NUMBERS = 0
@@ -37,73 +38,20 @@ for i in choises.values():
 
 
 
-""" def get_ready():
-    global antall_spillere
-    global choises
-    
-    chosen_tickets = []
-
-    choise = " "
-    while antall_spillere == 0:
-        try:
-            antall_spillere = int(input("hvor mange spillere? "))
-        except ValueError:
-            print("Hei du må bruke et tall da!!")
-    i=0
-    print(antall_spillere)
-
-    while i < antall_spillere:
-        i+=1
-        try:
-            name = str(input("Skriv navnet ditt: "))
-        except ValueError:
-            print("")
-        choises[name] = []
-        lodd = []
-        choise = -1
-        while choise != 0:
-            try:
-                choise = int(input(f"Velg et tall mellom {start} og {stop}: "))
-            except ValueError:
-                print("wow wow wow det må være et tall")
-            if (choise not in chosen_tickets) & (choise !=0) & (choise < stop) & (choise > start):
-                print(f"{name} har kjøpt et lodd")
-                choises[name].append(choise)
-                chosen_tickets.append(choise)
-                print(choises)
-            elif choise in chosen_tickets:
-                print("Allerede valgt, prøv igjen")
-
-            if choise == 0:
-                print("ferdig valgt")
-
-            elif (choise > stop or choise < start):
-                print(f"loddene må være mellom {start} og {stop}") """
-
-
 def chose_winner(font, screen, bg, width):
     global ranking
     global NUMBERS
-    winners = 2 #math.ceil(antall_spillere/2)
+    winners = 2 
     winner = 0
     teller = 0
     sleep = 1
     long_sleep = 1
-    """ for i in range(21):
-        remove_one_number(font, screen, bg, width, 0.1) """
 
     while NUMBERS > 2:
         teller += 1
         lottery_winners = []
         i=0
         text4 = ""
-
-        """ while i < winners:
-            i+=1
-            tall = random.randint(1,len(numbers)-1)
-            lottery_winners.append(numbers[tall])
-            numbers.remove(numbers[tall]) """
-
 
         if teller % 2 == 0:
             remove_one_number(font, screen, bg, width, long_sleep)
@@ -131,26 +79,6 @@ def chose_winner(font, screen, bg, width):
             py.display.update()
             time.sleep(10000)
             
-            
-        """else:
-            print(f"vinnertallene er {lottery_winners}")
-            for key, value in choises.items():
-                for w in lottery_winners:
-                    if w in choises.get(key):
-                        winner +=1
-                        text4 = f"{winner}. plass er {key} med vinnertallet {w}"
-                        print(f"{winner}. plass er {key} med vinnertallet {w}")
-                        info = (winner, key, w)
-                        print(key)
-                        ranking.add(info)
-                            
-                    else:
-                        text4 = " Ingen hadde det loddet"
-                        screen.blit(bg,(0,0))
-                        text2 = f"Vinnertallene er: {lottery_winners}"
-                        bli_text_to_screen(text2,font, screen, bg, width, 100)
-                        show_tickets(font, screen, bg)
-                        py.display.update() """
         time.sleep(sleep)
         if winner != winners:
             text3 = "EN TIL, EN TIL, VI REKKER EN TIL"
@@ -209,7 +137,6 @@ def bli_text_to_screen(text,font, screen, bg, width, hight = 0):
     text = font.render(text, True, colour1, colour2)
     textREct = text.get_rect()
     textREct.center = (width // 2, hight)
-    #screen.blit(bg,(0,0))
     screen.blit(text, textREct)        
             
 
@@ -280,6 +207,5 @@ def the_prankster(font, screen, bg, width, hight):
     time.sleep(1)
 
 if __name__ == "__main__":
-    #get_ready()
     show_pygame()
     
